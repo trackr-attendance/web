10511a1376b6396df93d448a551b9e0592bbbd6c
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2017-11-13 13:23:36 -0500

    Roughed in Handlebars Templates for Onboarding

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.set('view engine', 'handlebars');
 app.get('/', routes.notImplementedResponse);
 
 /* On Boarding Workflow */
-app.get('/onboarding/welcome/', routes.notImplementedResponse);
-app.get('/onboarding/class/', routes.notImplementedResponse);
-app.get('/onboarding/roster/', routes.notImplementedResponse);
-app.get('/onboarding/faces/', routes.notImplementedResponse);
-app.get('/onboarding/finished/', routes.notImplementedResponse);
+app.get('/onboarding/welcome/', routes.onboarding.welcome);
+app.get('/onboarding/class/', routes.onboarding.class);
+app.get('/onboarding/roster/', routes.onboarding.roster);
+app.get('/onboarding/faces/', routes.onboarding.faces);
+app.get('/onboarding/finished/', routes.onboarding.finished);
 
 var port = 8080;
 app.listen(port, function(){
--- routes.js
+++ routes.js
@@ -1,4 +1,31 @@
 exports.notImplementedResponse = function(req, res){
     console.log('[INFO] Recieved request at ', req.url);
     res.status(501).send({"status code": 501, "status string": "501 Not Implemented", "message": req.url + " is not implemented."});
-}
+};
+
+exports.onboarding = {};
+
+exports.onboarding.welcome = function(req, res){
+    console.log('[INFO] Recieved request at ', req.url);
+    res.render('onboarding/welcome');
+};
+
+exports.onboarding.class = function(req, res){
+    console.log('[INFO] Recieved request at ', req.url);
+    res.render('onboarding/class');
+};
+
+exports.onboarding.roster = function(req, res){
+    console.log('[INFO] Recieved request at ', req.url);
+    res.render('onboarding/roster');
+};
+
+exports.onboarding.faces = function(req, res){
+    console.log('[INFO] Recieved request at ', req.url);
+    res.render('onboarding/faces');
+};
+
+exports.onboarding.finished = function(req, res){
+    console.log('[INFO] Recieved request at ', req.url);
+    res.render('onboarding/finished');
+};
