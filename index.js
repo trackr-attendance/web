fc036828c960264383bcb5f230c0dc426ddf5ccd
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2017-11-13 11:36:46 -0500

    Added OnBoarding Routes

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ var app = express();
 /* Routing */
 app.get('/', routes.notImplementedResponse);
 
+/* On Boarding Workflow */
+app.get('/onboarding/welcome/', routes.notImplementedResponse);
+app.get('/onboarding/class/', routes.notImplementedResponse);
+app.get('/onboarding/roster/', routes.notImplementedResponse);
+app.get('/onboarding/faces/', routes.notImplementedResponse);
+app.get('/onboarding/finished/', routes.notImplementedResponse);
+
 var port = 8080;
 app.listen(port, function(){
     console.log('[INFO] Listening at: http://127.0.0.1:8080');
