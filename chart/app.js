525803e365ccea9eb397f7a6f0d4c907befdd4ed
aptmoniker <andrewwallacefoster@gmail.com>
2017-12-01 15:55:33 -0500

    Draw Line Chart For Engagement Score

---
+++ chart/app.js
@@ -0,0 +1,117 @@
+function addAxesAndLegend (svg, xAxis, yAxis, margin, chartWidth, chartHeight) {
+
+  var axes = svg.append('g')
+    .attr('clip-path', 'url(#axes-clip)');
+
+  axes.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + chartHeight + ')')
+    .call(xAxis)
+
+  axes.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis)
+    .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '.71em')
+      .style('text-anchor', 'end')
+      .text('Avg Engagement Score');
+}
+
+function drawPaths (svg, data, x, y) {
+  var medianLine = d3.svg.line()
+    .interpolate('basis')
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.pct50); });
+
+  svg.datum(data);
+
+  svg.append('path')
+    .attr('class', 'median-line')
+    .attr('d', medianLine)
+    .attr('clip-path', 'url(#rect-clip)');
+}
+
+function startTransitions (svg, chartWidth, chartHeight, rectClip, markers, x) {
+  rectClip.transition()
+    .duration(1000*markers.length)
+    .attr('width', chartWidth);
+
+  markers.forEach(function (marker, i) {
+    setTimeout(function () {
+      addMarker(marker, svg, chartHeight, x);
+    }, 1000 + 500*i);
+  });
+}
+
+function makeChart (data, markers) {
+  var svgWidth  = 960,
+      svgHeight = 500,
+      margin = { top: 20, right: 20, bottom: 40, left: 40 },
+      chartWidth  = svgWidth  - margin.left - margin.right,
+      chartHeight = svgHeight - margin.top  - margin.bottom;
+
+  var x = d3.time.scale().range([0, chartWidth])
+            .domain(d3.extent(data, function (d) { return d.date; })),
+      y = d3.scale.linear().range([chartHeight, 0])
+            .domain([0, d3.max(data, function (d) { return d.pct95; })]);
+
+  var xAxis = d3.svg.axis().scale(x).orient('bottom')
+                .innerTickSize(-chartHeight).outerTickSize(0).tickPadding(10),
+      yAxis = d3.svg.axis().scale(y).orient('left')
+                .innerTickSize(-chartWidth).outerTickSize(0).tickPadding(10);
+
+  var svg = d3.select('body').append('svg')
+    .attr('width',  svgWidth)
+    .attr('height', svgHeight)
+    .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+  // clipping to start chart hidden and slide it in later
+  var rectClip = svg.append('clipPath')
+    .attr('id', 'rect-clip')
+    .append('rect')
+      .attr('width', 0)
+      .attr('height', chartHeight);
+
+  addAxesAndLegend(svg, xAxis, yAxis, margin, chartWidth, chartHeight);
+  drawPaths(svg, data, x, y);
+  startTransitions(svg, chartWidth, chartHeight, rectClip, markers, x);
+}
+
+var parseDate  = d3.time.format('%Y-%m-%d').parse;
+d3.json('data.json', function (error, rawData) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  var data = rawData.map(function (d) {
+    return {
+      date:  parseDate(d.date),
+      pct05: d.pct05 / 1000,
+      pct25: d.pct25 / 1000,
+      pct50: d.pct50 / 1000,
+      pct75: d.pct75 / 1000,
+      pct95: d.pct95 / 1000
+    };
+  });
+
+  d3.json('markers.json', function (error, markerData) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    var markers = markerData.map(function (marker) {
+      return {
+        date: parseDate(marker.date),
+        type: marker.type,
+        version: marker.version
+      };
+    });
+
+    makeChart(data, markers);
+  });
+});
